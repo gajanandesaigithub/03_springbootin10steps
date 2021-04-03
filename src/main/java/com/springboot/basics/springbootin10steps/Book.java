7ebd6a19e951750b0e4688c9918f7a9b8dc829b9
gajanandesaigithub <gajanan.desai@gmail.com>
2021-04-03 13:11:10 +0530

    test

---
+++ src/main/java/com/springboot/basics/springbootin10steps/Book.java
@@ -0,0 +1,28 @@
+package com.springboot.basics.springbootin10steps;
+
+public class Book {
+	long id;
+	String name;
+	String auther;
+	public Book(long id, String name, String auther) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.auther = auther;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAuther() {
+		return auther;
+	} 
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", auther=" + auther + "]";
+	}
+	
+
+}
